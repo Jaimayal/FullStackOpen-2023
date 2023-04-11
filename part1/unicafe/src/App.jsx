77c5c1fd62c21668488f2140683fc142b930f1e0
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-04-10 20:36:49 -0600

    Solved 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,15 +7,24 @@ function Button({ handleClick, text }) {
 }
 
 function Statistics({ good, neutral, bad, all }) {
+  if (all !== 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {all}</p>
+        <p>average: {(good - bad) / all}</p>
+        <p>positive: {good / all} %</p>
+      </>
+    )
+  }
+
   return (
     <>
       <h2>Statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {(good - bad) / all}</p>
-      <p>positive: {good / all} %</p>
+      <p>No feedback given</p>
     </>
   )
 }
