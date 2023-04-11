4fd6ed4e6d1e185e861e8c5d7d1bc377c9a5eccd
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-04-10 18:34:08 -0600

    Solved 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+function Button({ handleClick, text }) {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -7,7 +13,14 @@ function App() {
 
   return (
     <div>
-
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text={"good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+      <h2>Statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
     </div>
   )
 }
