10f3458fe309b2c403db5f13b6caa47f668a1f53
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-04-10 18:42:33 -0600

    Solved 1.7 unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,20 +7,39 @@ function Button({ handleClick, text }) {
 }
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  }
+
+  const handleBadCllick = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text={"good"} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
-      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+      <Button handleClick={handleGoodClick} text={"good"} />
+      <Button handleClick={handleNeutralClick} text={"neutral"} />
+      <Button handleClick={handleBadCllick} text={"bad"} />
       <h2>Statistics</h2>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {(good - bad) / all}</p>
+      <p>positive: {good / all} %</p>
     </div>
   )
 }
