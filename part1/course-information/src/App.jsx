77ee87f5f83fc6fdb6202eba98c96710d443660d
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-05-20 08:43:44 -0600

    Updated README

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -1,66 +1,45 @@
+import Course from "./components/Course";
 
-function Header({ course }) {
-  return (
-    <>
-      <h1>{course}</h1>
-    </>
-  )
-}
 
-function Content({ parts }) {
-  return (
-    <>
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
-    </>
-  )
-}
-
-function Part({ part, exercises }) {
-  return (
-    <>
-      <p>
-        {part} {exercises}
-      </p>
-    </>
-  )
-}
-
-function Total({ parts }) {
-  return (
-    <>
-      <p>Number of exercises {parts.reduce((acc, elem) => acc + elem.exercises, 0)}</p>
-    </>
-  )
-}
 function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+        },
+        {
+          name: 'Redux',
+          exercises: 21,
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'SQL',
+          exercises: 10,
+        },
+        {
+          name: 'File IO',
+          exercises: 5,
+        },
+      ]
+    },
+  ]
 
-
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
+  return courses.map(course => <Course course={course} />);
 }
 
 export default App
---
+++ part1/course-information/src/components/Course.jsx
@@ -0,0 +1,42 @@
+
+function Header({ course }) {
+    return (
+      <>
+        <h1>{course}</h1>
+      </>
+    )
+}
+  
+function Content({ parts }) {
+    return parts.map(part => <Part part={part.name} exercises={part.exercises} />)
+}
+  
+function Part({ part, exercises }) {
+    return (
+      <>
+        <p>
+          {part} {exercises}
+        </p>
+      </>
+    )
+}
+  
+  function Total({ parts }) {
+    return (
+      <>
+        <p>Number of exercises {parts.reduce((acc, elem) => acc + elem.exercises, 0)}</p>
+      </>
+    )
+  }
+  
+  function Course({ course }) {
+    return (
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    )
+  }
+
+  export default Course
