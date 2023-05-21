b224070b1cf63392ff95b6f537fef89da30939b1
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-05-20 23:18:47 -0600

    Solved Exercise 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SearchBar({ searchValue, onSearchInputChange }) {
 	return (
@@ -48,16 +49,22 @@ function PersonsList({ persons, searchValue }) {
 }
 
 function App() {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [searchValue, setSearchValue] = useState("");
 
+	useEffect(() => {
+		console.log("Effect");
+		axios
+			.get("http://localhost:3001/persons")
+			.then((res) => {
+				console.log("setting persons");
+				console.log(res.data);
+				setPersons(res.data);
+			});
+	}, []);
+	
 	const onNameInputChange = (event) => {
 		const updatedValue = event.target.value;
 		setNewName(updatedValue);
